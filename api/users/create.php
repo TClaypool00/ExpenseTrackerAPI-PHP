9f424a05284ef42723d3069f5b5e08d691d6f507
Tyler <claypooltyler00@gmail.com>
2020-09-14 01:59:39 -0500

    can finnally post a user!

--- api/users/create.php
+++ api/users/create.php
@@ -1,43 +1,30 @@
 <?php
 // required headers
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+header("Content-Type: application/json;");
 header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods, Content-Type, Authorization, X-Requested-Width");
 
 // get database connection
-include_once "../../config/database.php";
-include_once "../objects/users.php";
+include_once "../../config/Database.php";
+include_once "../../models/users.php";
 
 $database = new Database();
-$db = $database->getConnection();
+$db = $database->connect();
 
 $user = new Users($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
-// First make sure data is empty
-if (
-    !empty($data->FirstName) &&
-    !empty($data->LastName) &&
-    !empty($data->EmailAddress) &&
-    !empty($data->Password) &&
-    !empty($data->IsAdmin) &&
-    !empty($data->Address) &&
-    !empty($data->City) &&
-    !empty($data->State) &&
-    !empty($data->Zip)
-) {
-    $user->FirstName = $data->FirstName;
-    $user->LastName = $data->LastName;
-    $user->EmailAddress = $data->EmailAddress;
-    $user->Password = $data->Password;
-    $user->IsAdmin = $dta->IsAdmin;
-    $user->Address = $data->Address;
-    $user->City = $data->City;
-    $user->State = $data->State;
-    $user->Zip = $data->Zip;
+    $user->firstName = $data->firstName;
+    $user->lastName = $data->lastName;
+    $user->email = $data->email;
+    $user->password = $data->password;
+    $user->isAdmin = $data->isAdmin;
+    $user->address = $data->address;
+    $user->city = $data->city;
+    $user->state = $data->state;
+    $user->zip = $data->zip;
 
     // Create User
     if ($user->create()) {
@@ -46,26 +33,13 @@ if (
         http_response_code(201);
 
         // tell the user
-        echo json_encode(array("message" => "Product was created."));
-    }
-
-    // if unable to create the product, tell the user
-    else {
+        echo json_encode(array("message" => "User was created."));
+    } else {
 
         // set response code - 503 service unavailable
         http_response_code(503);
 
         // tell the user
-        echo json_encode(array("message" => "Unable to create product."));
+        echo json_encode(array("message" => "Unable to create user."));
     }
-}
 
-// tell the user data is incomplete
-else {
-
-    // set response code - 400 bad request
-    http_response_code(400);
-
-    // tell the user
-    echo json_encode(array("message" => "Unable to create product. Data is incomplete."));
-}
